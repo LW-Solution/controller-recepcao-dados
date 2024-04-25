7c9e42ad9433f7239cd983b7c5349784bfaad095
Miguel <doxito007@gmail.com>
2024-04-25 10:55:05 -0300

    Testando Banco Redis

--- src/sub.py
+++ src/sub.py
@@ -3,6 +3,8 @@
 import json
 import paho.mqtt.client as mqtt
 import conectRedis
+import uuid
+
 
 r = conectRedis.conectar()
 
@@ -18,10 +20,23 @@ def on_connect(con, userdata, flags, rc, properties):
 def on_message(con, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     payload_str = msg.payload.decode('utf-8')  # decode bytes to string
-    payload_json = json.loads(payload_str)  # convert string to JSON
-    print("Jason TESTE:")
-    print(payload_json)
-    print(type(payload_json))
+    payload_json = json.dumps(payload_str)  # convert string to JSON
+    chave = str(uuid.uuid4())
+    r.set(chave, payload_json)
+    chaves = r.keys('*')
+    
+    # Lista para armazenar os JSONs
+    dados_json = []
+
+    for chave in chaves:
+    # Recuperando o valor (JSON) associado à chave
+        valor = r.get(chave)
+    
+    # Decodificando o valor de JSON para um dicionário Python
+        json_data = json.loads(valor)
+    
+    # Adicionando o JSON à lista de dados
+        dados_json.append(json_data)
 
     
     
